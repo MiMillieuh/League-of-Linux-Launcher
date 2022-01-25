46a89390034ce0b3331745e8947304b9e68b5e41
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2022-01-25 23:13:32 +0100

    Add files via upload

--- leagueoflinuxinsall.sh
+++ leagueoflinuxinsall.sh
@@ -1,9 +1,9 @@
 #!bin/bash
-DXVKver="1.9.3"
-DXVK=https://github.com/doitsujin/dxvk/releases/download/v$DXVKver/dxvk-$DXVKver.tar.gz
+#DXVKver="1.9.3"
+#DXVK=https://github.com/doitsujin/dxvk/releases/download/v$DXVKver/dxvk-$DXVKver.tar.gz
 WINELOLtag="7.0-GE-1-LoL"
-WINELOLver="wine-lutris-ge-7.0-1-lol"
-WINELOL="https://github.com/GloriousEggroll/wine-ge-custom/releases/download/$WINELOLtag/$WINELOLver-x86_64.tar.xz"
+WINELOLver="lutris-ge-7.0-1-lol"
+WINELOL="https://github.com/GloriousEggroll/wine-ge-custom/releases/download/$WINELOLtag/wine-$WINELOLver-x86_64.tar.xz"
 
 
-bash -c "mkdir ~/.LeagueOfLegends ; cd ~/.LeagueOfLegends ; wget https://lutris.nyc3.digitaloceanspaces.com/games/league-of-legend/launchhelper.sh ; wget https://lutris.nyc3.digitaloceanspaces.com/games/league-of-legend/syscall_check.sh ; wget $WINELOL ; tar -xf $WINELOLver-x86_64.tar.xz ; mkdir prefix ; cd lutris-* ; WINEPREFIX=~/.LeagueOfLegends/prefix /bin/wineboot ; cd .. ; wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks ; chmod +x winetricks ; ./winetricks dxvk ; mkdir ~/.LeagueOfLegends/gamedl ; cd ~/.LeagueOfLegends/gamedl ; wget https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.na.exe ; cd .. ; cd lutris-* ; WINEPREFIX=~/.LeagueOfLegends/prefix /bin/wine ../gamedl/live.na.exe"
+bash -c "mkdir ~/.LeagueOfLegends ; cd ~/.LeagueOfLegends ; wget https://lutris.nyc3.digitaloceanspaces.com/games/league-of-legend/launchhelper.sh ; wget https://lutris.nyc3.digitaloceanspaces.com/games/league-of-legend/syscall_check.sh ; wget $WINELOL ; tar -xf wine-$WINELOLver-x86_64.tar.xz ; mkdir prefix ; WINEPREFIX=~/.LeagueOfLegends/prefix $WINELOLver-x86_64/bin/wineboot ; wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks ; chmod +x winetricks ; ./winetricks --silent dxvk windowmanagerdecorated=n vcrun2012 vcrun 2010 winhttp vcrun2015 corefont win7 ; mkdir ~/.LeagueOfLegends/gamedl ; cd ~/.LeagueOfLegends/gamedl ; wget https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.na.exe ; cd ~/.LeagueOfLegends/ ; WINEPREFIX=~/.LeagueOfLegends/prefix $WINELOLver-x86_64/bin/wine gamedl/live.na.exe"
