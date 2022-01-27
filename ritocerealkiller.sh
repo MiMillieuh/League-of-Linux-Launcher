bf0a4386dc4efe995ca9e4ead07349512f53b721
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2022-01-27 18:15:15 +0100

    Update ritocerealkiller.sh

--- ritocerealkiller.sh
+++ ritocerealkiller.sh
@@ -4,6 +4,9 @@ if pgrep -x "RiotClientServices.exe" > /dev/null
 then
     sleep 5 
     killall RiotClientServices.exe
+    sleep 5
+    WINEPREFIX=~/.LeagueOfLegends/prefix winelol/bin/wine "prefix/drive_c/Riot Games/Riot Client/RiotClientServices.exe" --launch-product=league_of_legends --launch-patchline=live
+    
 else
     echo "Stopped"
 fi
