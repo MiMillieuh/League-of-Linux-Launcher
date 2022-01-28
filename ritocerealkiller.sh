f252eade7e6951ce681a53a3957252485d65af9c
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2022-01-28 11:36:50 +0100

    Update ritocerealkiller.sh

--- ritocerealkiller.sh
+++ ritocerealkiller.sh
@@ -3,7 +3,7 @@
 cerealkiller () {
 if pgrep -x "RiotClientUx.exe" > /dev/null
 then
-    RiotClientUx.exe && sleep 10 && mv ~/.LeagueOfLegends/lutris* ~/.LeagueOfLegends/winelol && WINEPREFIX=~/.LeagueOfLegends/prefix ~/.LeagueOfLegends/winelol/bin/wine "~/.LeagueOfLegends/prefix/drive_c/Riot Games/Riot Client/RiotClientServices.exe" --launch-product=league_of_legends --launch-patchline=live
+    killall RiotClientUx.exe && sleep 10 && mv ~/.LeagueOfLegends/lutris* ~/.LeagueOfLegends/winelol && WINEPREFIX=~/.LeagueOfLegends/prefix ~/.LeagueOfLegends/winelol/bin/wine "~/.LeagueOfLegends/prefix/drive_c/Riot Games/Riot Client/RiotClientServices.exe" --launch-product=league_of_legends --launch-patchline=live
 else
 cerealkiller
 fi
